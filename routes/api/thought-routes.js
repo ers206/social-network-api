3a6a60c82a5cb955fbaeb5cfdeb7a5b0a1d77082
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-09-24 13:46:51 -0500

    using mod as template

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,3 +1,30 @@
+// from pizzahunt comments will be changed to thoughts 
+
+const router = require('express').Router();
+const {
+  addComment,
+  removeComment,
+  addReply,
+  removeReply
+} = require('../../controllers/comment-controller');
+
+// /api/comments/<pizzaId>
+router.route('/:pizzaId').post(addComment);
+
+// /api/comments/<pizzaId>/<commentId>
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment);
+
+// /api/comments/<pizzaId>/<commentId>/<replyId>
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
+
+module.exports = router;
+
+
+
+
 // /api/thoughts
 
 // GET to get all thoughts
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,3 +1,29 @@
+// from pizzahunt change pizza to user 
+
+const router = require('express').Router();
+const {
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+  } = require('../../controllers/pizza-controller');
+
+router
+.route('/')
+.get(getAllPizza)
+.post(createPizza);
+
+// Set up GET one, PUT, and DELETE at /api/pizzas/:id
+// /api/pizzas/:id
+router
+.route('/:id')
+.get(getPizzaById)
+.put(updatePizza)
+.delete(deletePizza);
+
+module.exports = router;
+
 // /api/users
 
 // GET all users
